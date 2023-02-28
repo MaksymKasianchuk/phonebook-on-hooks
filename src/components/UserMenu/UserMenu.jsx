39fabf3bd7c87599c1e8aa3d32b373fcda86d001
Maksym <maxthebest1906@gmail.com>
2023-02-28 12:59:40 +0200

    clear contacts store for another user before logout

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,12 +1,14 @@
 import { useAuth } from "hooks";
 import { useDispatch } from "react-redux";
 import { logOut } from "redux/auth/operations";
+import { contactsApi } from 'redux/contacts/contactsApi';
 
 const UserMenu = () => {
     const { user } = useAuth();
     const dispatch = useDispatch();
 
     const handleLogOut = async () => {
+        await dispatch(contactsApi.util.resetApiState()); //clear contacts for another user
         await dispatch(logOut());
     };
 
